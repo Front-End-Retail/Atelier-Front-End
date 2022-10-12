3e5ba584049e8c878a622207e146bc8911210c43
Andrew Dye <phodye@gmail.com>
2022-10-12 14:43:49 -0400

    Added file structure for basic widgets

---
+++ client/src/RelatedItemsAndComparison/RelatedItemsAndComparison.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const { useState, useEffect } = React;
+
+const RelatedItemsAndComparison = () => {
+
+  return (
+    <div>
+      Questions And Answers
+    </div>
+  )
+}
+
+export default RelatedItemsAndComparison;
