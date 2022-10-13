57bb7766baabb271f253e58c188430d385b3049b
Andrew Dye <phodye@gmail.com>
2022-10-13 17:33:52 -0400

    Added more research in notes on QA module for axios requests

--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.js
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.js
@@ -4,9 +4,14 @@ const { useState, useEffect } = React;
 
 const QuestionsAndAnswers = () => {
 
+  const = tryARequest => {
+
+  }
+
   return (
     <div>
       Questions And Answers
+      <button onClick={()=>{}}>Test Button</button>
     </div>
   )
 }
@@ -22,9 +27,14 @@ Atelier API always requires Github token in Authorization in header
 List Questions
 GET /qa/questions
 
-The parameters for an axious request to QandA
-Parameter	    Type	  Description
-product_id	integer	Specifies the product for which to retrieve questions.
-page	integer	Selects the page of results to return. Default 1.
-count	integer	Specifies how many results per page to return. Default 5.
+A semi succesful QA get request via postman for question list:
+https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions?product_id=5
+A succesful QA get request for a product with a real id
+https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions?product_id=37311
+
+37311 to 37315
+
+A semi succesful QA get request via postman for answers:
+https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/5/answers
+
 */
