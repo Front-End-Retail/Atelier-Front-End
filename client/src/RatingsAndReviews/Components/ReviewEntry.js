cff2ca82afa317a6445a04afb03c48bcfff85f45
Maxwell Philip <max.philip1@gmail.com>
2022-10-14 11:33:29 -0400

    Add skeleton components to Ratings/reviews, change access-origin header in server to only allow localhost/3001

---
+++ client/src/RatingsAndReviews/Components/ReviewEntry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const axios = require('axios');
+const { useState, useEffect } = React;
+
+const ReviewEntry = () => {
+  return (
+    <div>
+      Review Entry
+    </div>
+  )
+}
+
+export default ReviewEntry
