c9d8f337b51012ffc8b78c5c47078dba92e14326
Andrew Dye <phodye@gmail.com>
2022-10-14 19:18:18 -0400

    Added date-fns library and implemented in QAAnswerItem

--- client/src/QuestionsAndAnswers/QAAnswerItem.js
+++ client/src/QuestionsAndAnswers/QAAnswerItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { format, parseISO } from "date-fns";
 const axios = require('axios');
 
 const { useState, useEffect } = React;
 
 const QAAnswerItem = ({ answer }) => {
+
+  //console.log(answer, answer.date, format(parseISO(answer.date), 'MMMM d, yyyy'))
 
   return (
     <div className={"qalist-aline-wrapper"}>
@@ -12,7 +15,7 @@ const QAAnswerItem = ({ answer }) => {
           <p className={"qalist-a-text"}>{answer.body}</p>
         </div>
         <div className={"qalist-answer-info-wrapper"}>
-          <p>by {answer.answerer_name}, January 1, 2019</p>
+          <p>by {answer.answerer_name}, {format(parseISO(answer.date), 'MMMM d, yyyy')}</p>
           <p> | </p>
           <p>Helpful?</p>
           <p>Yes({answer.helpfulness})</p>
