1940f63e75140d4137b6750cbb811bd9c8f3ade2
Maxwell Philip <max.philip1@gmail.com>
2022-10-14 20:39:24 -0400

    changing back into ratings branch

--- client/src/RatingsAndReviews/Components/Sorting.js
+++ client/src/RatingsAndReviews/Components/Sorting.js
@@ -4,7 +4,13 @@ const { useState, useEffect } = React;
 
 const Sorting = ({review}) => {
   return (
+    <select name="languages" id="lang">
+      <option value="none">None Selected</option>
+      <option value="helpful">Helpful</option>
+      <option value="newest">Newest</option>
+      <option value="relevant">Relevant</option>
 
+  </select>
     // <div>
     // </div>
   )
---
+++ client/src/RelatedItemsAndComparison/RelatedItemsAndComparison.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+import RelatedProducts from './RelatedProducts.jsx';
+import YourOutfit from './YourOutfit.jsx';
+
+const { useState, useEffect } = React;
+
+
+const RelatedItemsAndComparison = () => {
+  const handleClick = (event) => {
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products', {headers})
+    .then((response)=>{
+      const allProducts = response.data;
+      console.log('all products client received', allProducts);
+    })
+    .catch((err)=> {
+      console.log('client failed to retrieve all products', err);
+    })
+  };
+
+  return (
+    <div>
+      Related Items and Comparison go here!
+      <br></br>
+      <button onClick={handleClick}>test</button>
+      <br></br>
+      <span>You may also like</span>
+      <RelatedProducts></RelatedProducts>
+      <span>Your outfit</span>
+      <YourOutfit></YourOutfit>
+    </div>
+  )
+}
+
+export default RelatedItemsAndComparison;
