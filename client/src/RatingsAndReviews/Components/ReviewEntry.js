ac2fb2c8d73e8fc3eeddab131f4b09fdcf4f385b
Maxwell Philip <max.philip1@gmail.com>
2022-10-18 12:43:26 -0400

    add image modal and refactor get request for reviews to pull all reviews (count=500). refactor sort to use api

--- client/src/RatingsAndReviews/Components/ReviewEntry.js
+++ client/src/RatingsAndReviews/Components/ReviewEntry.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RandRImage from './RandRImage.js'
 const axios = require('axios');
 const { useState, useEffect } = React;
 
@@ -9,8 +10,9 @@ const ReviewEntry = ({review}) => {
       <h4>{review.summary}</h4>
       <p>{review.date}</p>
       <p>{review.body}</p>
-      {review.photos.length > 0 && review.photos.map(photo => {
-        return <img className="thumbnail" src={photo.url}></img>
+      {review.photos.length > 0 && review.photos.map((photo, index) => {
+        return <RandRImage photo={photo} key ={index}/>
+        // return <img className="thumbnail" src={photo.url}></img>
       })}
       {review.recommend && <p>I recommend this product!</p>}
       <p>Helpful?<a className="review-links">Yes</a><a className="review-links toggle-line">Report</a></p>
