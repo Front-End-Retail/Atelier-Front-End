ba667c0c37e7d53d03aec835eb7ffcf04b6b948c
Lei Liang <m.leiliang@gmail.com>
2022-10-20 18:29:55 -0400

    modify html and css for each outfit

--- client/src/RelatedItemsAndComparison/YourOutfit.jsx
+++ client/src/RelatedItemsAndComparison/YourOutfit.jsx
@@ -10,17 +10,23 @@ const YourOutfit = ({outfit, updateOutfitList}) =>{
 
 
   return (
-    <div className='currentOutfit'>
-      <div className='timesIcon' onClick={()=>{updateOutfitList(outfit.styleID)}}>
-      <FontAwesomeIcon icon={faTimes}/>
-      </div>
-      <div>{outfit.name}</div>
-      <div>{outfit.style}</div>
-      <div>{outfit.category}</div>
-      {/* <div>{outfit.styleID}</div>  I dont want to display this to users but i need it*/}
-      <div className='regularPrice'>${outfit.regularPrice}</div>
-      <div style={{ color: 'red' }}>${outfit.price}</div>
-      <img src={outfit.image} alt='outfit.image' height='123px' width='155px'/>
+    <div className='outfit-component'>
+         <div className='outfit-pic' style={{
+               backgroundImage: `url(${outfit.image})`,
+               backgroundSize: 'cover',
+               backgroundPosition: 'center'}} alt='your outfit'>
+               {/* <img src={outfit.image} alt='outfit.image' height='123px' width='155px'/> */}
+               <div className='timesIcon' onClick={()=>{updateOutfitList(outfit.styleID)}}>
+                    <FontAwesomeIcon icon={faTimes}/>
+               </div>
+          </div>
+         <div className='outfit-info'>
+               <div className='outfit-name'>{outfit.name}</div>
+               <div className='outfit-style'>{outfit.style}</div>
+               <div className='outfit-category'>{outfit.category}</div>
+               {/* <div>{outfit.styleID}</div>  I dont want to display this to users but i need it*/}
+              <span className='outfit-regularPrice'>USD {outfit.regularPrice}</span><span className='outfit-discountPrice' style={{ color: 'red' }}>USD {outfit.price}</span>
+         </div>
     </div>
   );
 }
