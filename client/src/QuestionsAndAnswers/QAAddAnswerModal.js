1906a775078d3938dedbd049d4a543cccbd70e8f
Andrew Dye <phodye@gmail.com>
2022-10-21 16:21:52 -0400

    Styling added

--- client/src/QuestionsAndAnswers/QAAddAnswerModal.js
+++ client/src/QuestionsAndAnswers/QAAddAnswerModal.js
@@ -5,8 +5,8 @@ import QAAnswerSubmitForm from './QAAnswerSubmitForm.js'
 const container = document.getElementById('root');
 
 const QAAddAnswerModal = ({ visible, toggle, currentProductName, questionText, questionId, getProductQuestions }) => visible ? ReactDOM.createPortal(
-  <div className="modal">
-    <div className="modal-pop" role="dialog" aria-modal="true">
+  <div className="qa-modal">
+    <div className="modal-pop-qa" role="dialog" aria-modal="true">
       <h2 className="modal-title">Submit Your Answer</h2>
       <h3 className="modal-subtitle">{currentProductName} : {questionText}</h3>
       <QAAnswerSubmitForm toggle={toggle} questionId={questionId} getProductQuestions={getProductQuestions} />
