510b64909760f6feef159a7a0f3fcddb0c47051d
Andrew Dye <phodye@gmail.com>
2022-10-23 15:14:53 -0400

    Fixed styling/icon on qasearch

--- client/src/QuestionsAndAnswers/QASearch.js
+++ client/src/QuestionsAndAnswers/QASearch.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import magnifyingGlass from '../assets/search.png';
 const axios = require('axios');
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 
 const { useState, useEffect } = React;
 
@@ -14,7 +17,7 @@ const QASearch = ({ newSearchTerm }) => {
   return (
     <div className={'qasearchwrapper'}>
       <input value={searchText} onChange={(event) => {setSearchText(event.target.value)}} type={"search"} id={"site-search"} name={"q"} className={"search-field"} placeholder={"HAVE A QUESTION? SEARCH FOR ANSWERS..."}></input>
-      <button type={"submit"} className={"search-button"}><img src={magnifyingGlass}></img></button>
+      <button type={"submit"} className={"search-button"}><FontAwesomeIcon icon={faMagnifyingGlass} className={"magnifying-glass"}></FontAwesomeIcon></button>
     </div>
   )
 }
