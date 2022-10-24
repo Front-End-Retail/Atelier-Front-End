981c46bb0e2aad5b68cf3b8a2ff553f876642469
Maxwell Philip <max.philip1@gmail.com>
2022-10-24 16:30:00 -0400

    search is working with filters!:wq

--- client/src/RatingsAndReviews/Components/ReviewList.js
+++ client/src/RatingsAndReviews/Components/ReviewList.js
@@ -4,22 +4,53 @@ const { useState, useEffect } = React;
 import ReviewEntry from './ReviewEntry.js';
 import Sorting from './Sorting.js';
 import ReviewModal from './ReviewModal.js';
-import SearchReviews from './SearchReviews.js'
+import SearchReviews from './SearchReviews.js';
 import UseReviewModal from './UseReviewModal.js';
 
 const ReviewList = ({reviews, sortReviews, metaReviews, putRequest}) => {
   const [displayNum, setDisplayNum] = useState(2)
   const [currentReviews, setCurrentReviews] = useState([])
   const [searchReviews, setSearchReviews] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   const passSortingName = (name) => {
     sortReviews(name)
   }
+  // change reviews whenever reviews from parent change
   useEffect(() => {
-    setCurrentReviews(reviews.slice(0, displayNum))
-    setSearchReviews(reviews.slice(0, displayNum))
+      setCurrentReviews(reviews.slice(0, displayNum))
+    if (searchTerm.length < 3) {
+      setSearchReviews(reviews.slice(0, displayNum))
+    } else {
+      let searchedReviews = [...reviews]
+      searchedReviews = searchedReviews.filter(review => {
+        let reviewBody = review.body.toLowerCase()
+        if (reviewBody.indexOf(searchTerm) !== -1) {
+          return review
+        }
+      })
+      console.log('searching...', searchedReviews)
+      setSearchReviews(searchedReviews.slice(0, displayNum))
+    }
+
   }, [reviews, displayNum])
 
+  // update search reviews using search term
+  useEffect(() => {
+    if (searchTerm.length < 3) {
+      setSearchReviews(currentReviews)
+    } else {
+      let searchedReviews = [...reviews]
+      searchedReviews = searchedReviews.filter(review => {
+        let reviewBody = review.body.toLowerCase()
+        if (reviewBody.indexOf(searchTerm) !== -1) {
+          return review
+        }
+      })
+      console.log('searching...', searchedReviews)
+      setSearchReviews(searchedReviews.slice(0, displayNum))
+    }
+  }, [searchTerm])
   const moreReviews = () => {
     if (displayNum + 1 < reviews.length) {
       setDisplayNum(displayNum + 2)
@@ -29,18 +60,23 @@ const ReviewList = ({reviews, sortReviews, metaReviews, putRequest}) => {
   }
   const toggleSearch = (term) => {
     if (term === false) {
-      setSearchReviews(currentReviews)
+      setSearchTerm('')
     } else {
-      let searchedReviews = [...reviews]
-      searchedReviews = searchedReviews.filter(review => {
-        let reviewBody = review.body.toLowerCase()
-        if (reviewBody.indexOf(term) !== -1) {
-          return review
-        }
-      })
-      console.log('searching...', searchedReviews)
-      setSearchReviews(searchedReviews)
+      setSearchTerm(term)
     }
+    // if (term === false) {
+    //   setSearchReviews(currentReviews)
+    // } else {
+    //   let searchedReviews = [...reviews]
+    //   searchedReviews = searchedReviews.filter(review => {
+    //     let reviewBody = review.body.toLowerCase()
+    //     if (reviewBody.indexOf(term) !== -1) {
+    //       return review
+    //     }
+    //   })
+    //   console.log('searching...', searchedReviews)
+    //   setSearchReviews(searchedReviews)
+    // }
   }
   const {toggle, visible} = UseReviewModal()
   return (
