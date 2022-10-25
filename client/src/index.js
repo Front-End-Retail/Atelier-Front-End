6020e63a7f34aa37f528ad6d5c766645202b47c0
Lei Liang <m.leiliang@gmail.com>
2022-10-25 00:18:47 -0400

    add footer and foot styling

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.js';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.js';
 import RelatedItemsAndComparison from './RelatedItemsAndComparison/RelatedItemsAndComparison.jsx'
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 //Dear Amazon/Google/Apple etc Don't lowball me.
 const App = () => {
@@ -81,6 +84,35 @@ const App = () => {
         <QuestionsAndAnswers currentProductID={currentProductID} currentProductName={currentProductName} />
         <RatingsAndReviews currentProductID={currentProductID} />
       </div>}
+      <footer className='footer'>
+        <div className='footer-container'>
+          <div className='list'>
+            <div className='company'>
+            <h3>Company</h3>
+                <a href='#'>about us</a>
+                <a href='#'>privacy policy</a>
+            </div>
+            <div className='get-help'>
+            <h3>Get help</h3>
+                <a href='#'>FAQ</a>
+                <a href='#'>shipping</a>
+                <a href='#'>returns</a>
+                <a href='#'>order status</a>
+                <a href='#'>payment options</a>
+            </div>
+            <div className='footer-email-form'>
+              <h3>Join our newsletter</h3>
+              <input type='email' placeholder='enter your email address' id='footer-email'></input>
+              <input type='submit' value='Sign Up' id='footer-email-btn' ></input>
+            </div>
+          </div>
+          <div className='social-links'><FontAwesomeIcon className='facebookIcon' icon={faFacebook} />
+            <FontAwesomeIcon className='twitterIcon' icon={faTwitter} />
+            <FontAwesomeIcon className='instaIcon' icon={faInstagram} />
+          </div>
+          <span className='trade-mark'>Atelier ® 2022</span>
+        </div>
+      </footer>
     </div>
   );
 }
