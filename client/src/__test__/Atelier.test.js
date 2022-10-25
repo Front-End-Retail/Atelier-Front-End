3012c99fb2244fa921eac1a737036cf507a1c1bf
Maxwell Philip <max.philip1@gmail.com>
2022-10-24 20:01:15 -0400

    change slider thumb color to gold

--- client/src/__test__/Atelier.test.js
+++ client/src/__test__/Atelier.test.js
@@ -1,17 +1,21 @@
-/**
- * @jest-environment jsdom
- */
-
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Overview from '../Overview/Overview.js';
+import RatingsAndReviews from '../RatingsAndReviews/RatingsAndReviews.js';
 test('loads items eventually', () => {
 
   render(<Overview />);
   it('should render the overview container to the DOM', () => {
     expect(screen.getByTestId('overview')).toBeInTheDocument();
   })
+})
+
+test('load the main container div', () => {
+
+  it('should return true for main randr div') {
+    expect(screen.getByTestId('randr-div'))
+  }
 })
 
