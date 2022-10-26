df9c7bea9f090fad008e72bf864fd820151be7d4
Parker Sturtevant <wsturtevant196@gmail.com>
2022-10-26 10:13:07 -0400

    Style checkmark

--- client/src/Overview/StyleSelector.jsx
+++ client/src/Overview/StyleSelector.jsx
@@ -9,15 +9,12 @@ const StyleSelector = ({ styles, selectedStyle, changeStyle }) => {
       <div className="style-thumbnails">
         {styles.map((style, i) => {
           return (
-            // <FontAwesomeIcon className="check-mark" icon={faCircleCheck} style={{ visibility: selectedStyle === style ? 'visible' : 'hidden' }} />
-            <div className="check-container">
-              <div data-testid="styles" className="image-container" style={{ borderColor: selectedStyle === style ? '#34699E' : '#fff' }}>
-                <img onClick={e => {
-                  e.preventDefault();
-                  changeStyle(style)
-                }
-                } className="style-image" src={style.photos[0].thumbnail_url} />
-              </div>
+            <div data-testid="styles" className="image-container" style={{ borderColor: selectedStyle === style ? '#34699E' : '#fff' }}>
+              <img onClick={e => {
+                e.preventDefault();
+                changeStyle(style)
+              }
+              } className="style-image" src={style.photos[0].thumbnail_url} />
             </div>
           )
         })}
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import Overview from './Overview/Overview.js';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.js';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.js';
-// import RelatedItemsAndComparison from './RelatedItemsAndComparison/RelatedItemsAndComparison.jsx'
+import RelatedItemsAndComparison from './RelatedItemsAndComparison/RelatedItemsAndComparison.jsx'
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
@@ -18,8 +18,7 @@ const App = () => {
   const [currentProductName, setCurrentProductName] = useState('');
   const [styles, setStyles] = useState([]);
   const [selectedStyle, setSelectedStyle] = useState({});
-  console.log(styles);
-  console.log(selectedStyle);
+
   //fetches initial product data and assigns the currentProductID state
   const fetchAllProducts = () => {
     axios({
