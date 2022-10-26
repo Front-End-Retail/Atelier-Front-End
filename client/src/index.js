b7ffe8b9939ecadf6116d401b5d42ccf1d97981c
Andrew Dye <phodye@gmail.com>
2022-10-26 12:22:37 -0400

    Updated header

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,11 @@ import { createRoot } from 'react-dom/client';
 import Overview from './Overview/Overview.js';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.js';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.js';
-// import RelatedItemsAndComparison from './RelatedItemsAndComparison/RelatedItemsAndComparison.jsx'
+import RelatedItemsAndComparison from './RelatedItemsAndComparison/RelatedItemsAndComparison.jsx'
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import LannisterLion from './assets/LannisterLion.png'
 
 
 //Dear Amazon/Google/Apple etc Don't lowball me.
@@ -78,7 +79,7 @@ const App = () => {
 
   return (
     <div className="lord-of-all-divs">
-      <header className={'logo'}><h1>Atelier</h1> <h2>Search ________</h2></header>
+      <header className={'logo'}><div className={'titleLogo'}><h1>One Stop Onesie Shop</h1><img src={LannisterLion}></img></div> <h2>Search ________</h2></header>
       {currentProductName !== '' && currentProductID !== 0 && <div>
         <Overview currentProductID={currentProductID} styles={styles} selectedStyle={selectedStyle} changeStyle={changeSelectedStyle} />
         <RelatedItemsAndComparison currentProductID={currentProductID} changeCurrentProduct={changeCurrentProduct} selectedStyle={selectedStyle} />
