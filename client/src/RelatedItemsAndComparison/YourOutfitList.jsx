2c6e2b34eefade9aae4eb5757f2af77c101b3640
Lei Liang <m.leiliang@gmail.com>
2022-10-28 09:46:53 -0400

    add dark shadow for star icon for accessibility

--- client/src/RelatedItemsAndComparison/YourOutfitList.jsx
+++ client/src/RelatedItemsAndComparison/YourOutfitList.jsx
@@ -153,39 +153,6 @@ if (styleIDList > 3) {
 
 export default YourOutfitList;
 
-// const YourOutfitList = ({outfitList, handlePlusIconClick, updateOutfitList}) => {
-
-//   if (outfitList.length === 0) {
-//     return (
-//      <div>
-//       <span className='emptyOutfitList'>Its empty here... Add this style?</span>
-//       <br></br>
-//       <br></br>
-//       <div onClick={handlePlusIconClick} className='plusIcon'>
-//       <FontAwesomeIcon icon={faPlus}/>
-//       </div>
-//      </div>
-//     )
-//   } else {
-//       return (
-//         <div className='yourOutfitListContainer'>
-//             <div className='addMoreOutfit'>
-//                  <p className='addMoreOutfit-word'>Add this style?</p>
-//                  <div onClick={handlePlusIconClick} className='plusIconInAddMore'>
-//                      <FontAwesomeIcon className='plus'icon={faPlus} />
-//                  </div>
-//             </div>
-//             {/* <button className='leftArrowIcon'><FontAwesomeIcon icon={faArrowLeft}/></button> */}
-//             {console.log('outfitList right before mapping: ', outfitList)}
-//             {outfitList.map((outfit, index)=>{
-//               return <YourOutfit key={index} outfit={outfit} updateOutfitList={updateOutfitList}></YourOutfit>
-//             })}
-
-//            {/* <button className='rightArrowIcon'><FontAwesomeIcon icon={faArrowRight}/></button> */}
-//         </div>
-//       );
-//     }
-//   };
 
 
 //just wrap a <div> and it would work---------------------
